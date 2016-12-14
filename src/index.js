e55fb4f3df76d971b9a3075a4d4e8c3a7edf15b6
mpietrzyk <mkpietrzyk@gmail.com>
2016-12-14 13:30:00 +0100

    Added NotFoundView. Routing WiP

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import App from './App/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={DashboardView}/>
+
+      <Route path="/students" component={StudentsView}>
+        <Route path="/students/:studentId" component={StudentView}/>
+      </Route>
+
+      <Route path="/courses" component={CoursesView}>
+        <Route path="/courses/:courseId" component={CourseView}/>
+      </Route>
+
+      <Route path="/samples" component={SamplesView}>
+        <Route path="/samples/counter" component={CounterView}/>
+      </Route>
+
+    </Route>
+    <Route path="*" component={NotFoundView}/>
+  </Router>,
   document.getElementById('root')
-);
+)
