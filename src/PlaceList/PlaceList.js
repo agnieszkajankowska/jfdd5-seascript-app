b704cf66a75e3bbc7296451611e2c0f9c37577f2
TomaszGola <gola92@gmail.com>
2016-12-15 15:51:59 +0100

    początek zabawy

--- src/PlaceList/PlaceList.js
+++ src/PlaceList/PlaceList.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+import {Link} from 'react-router'
+import {PlaceListItem} from'../PlaceListItem/'
 
-export default class extends React.Component {
-  constructor() {
-    super()
-    
-  }
-  render () {
-    return (
-      <div>
-        
-        <Button></Button>
-      </div>
-    )
-  }
-}
+export default (props) => (
 
+  <div>
+    <Link href='http://maps.google.com'>
+      <Button>Mapa</Button>
+    </Link>
+    <div>
+      <ul>
+        <li>
+          <PlaceListItem></PlaceListItem>
+        </li>
+      </ul>
+
+
+      <Link to='place-compare'>
+        <Button link>Compare</Button>
+      </Link>
+    </div>
+  </div>
+)
+   
