09b5bb4bc9eed70888f8dcbd90a97f15257c252c
ajankowska <ajankowska0404@gmail.com>
2016-12-16 13:33:21 +0100

    Creating items in comparer section

--- src/PlaceCompare/AttractionView/AttractionView.js
+++ src/PlaceCompare/AttractionView/AttractionView.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-import { places } from '../data'
+import {places} from '../data'
 
 export default class extends React.Component {
   render() {
     return (
       <div>
         <p>{this.props.attraction.price}</p>
-        <p>{places.atractions.filter(
-          waterSport => waterSport.indexOf(this.props.attraction.id) !== -1
-        ).map(place => place.name}}</p>
-
+        {places.atractions.filter(
+          waterSport =>waterSport.indexOf(this.props.attraction.id) !== -1
+        ).map(place => <p>{place.name}</p>)}
       </div>
     )
   }
 }
+
 
