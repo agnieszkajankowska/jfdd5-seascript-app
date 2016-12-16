b05160f0dcfea309c9a859cc57e7e378cc32d71b
Maciek <mb.maciejborkowski@gmail.com>
2016-12-16 08:43:39 +0100

    postawienie formularza z sugerowanymi wyborami/upgrade do developa

--- src/Database/index.js
+++ src/Database/index.js
@@ -1,5 +1,2 @@
-export { default as places } from './places'
-export { default as atraction } from './atraction'
-
 export { default as Places } from './places'
 export { default as Atraction } from './atraction'
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {FormGroup, ControlLabel, FormControl, Button, DropdownButton, MenuItem } from 'react-bootstrap'
 
-import { places, atraction } from '../Database'
+import { Places, Atraction } from '../Database'
 
 
 
@@ -24,12 +24,10 @@ export default class Form extends React.Component {
       atraction: ''
     }
   }
-
-
+  
 
   render() {
     return (
-      <form>
         <FormGroup onSubmit={this.handleSubmit}>
           <ControlLabel>Places</ControlLabel>
           <FormControl
@@ -43,7 +41,7 @@ export default class Form extends React.Component {
             placeholder="Enter place"
           />
           <DropdownButton key={1} title='Suggested places' id={`dropdown-basic-${1}`}>
-            {places.map(place =>
+            {Places.map(place =>
             <MenuItem eventKey="1">{place.name}</MenuItem>
             )}
           </DropdownButton>
@@ -60,13 +58,14 @@ export default class Form extends React.Component {
             placeholder="Enter atraction"
           />
           <DropdownButton key={2} title='Suggested atractions' id={`dropdown-basic-${1}`}>
-            {atraction.map(atraction =>
+            {Atraction.map(atraction =>
               <MenuItem eventKey="2">{atraction.name}</MenuItem>
             )}
           </DropdownButton>
+          <br />
+          <Button type="submit">Submit</Button>
         </FormGroup>
-        <Button type="submit">Submit</Button>
-      </form>
+
     )
   }
 }
