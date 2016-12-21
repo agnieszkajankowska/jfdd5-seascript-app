08ff5ae0c8d532f83b8c64eeab9472b1b8f0f738
agnieszkajankowska <ajankowska0404@gmail.com>
2016-12-21 21:17:57 +0100

    Corrected reducer to favorites and favorites display

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-export default (props) => (
-  <div>
-    <h1>Favorites</h1>
-  </div>
-)
+const mapStateToProps = state => ({
+  chosenToFavoritesAttractions: state.chosenAttractionsToFavoritesData.chosenToFavoritesAttractions
+})
 
+
+
+const Favorites = props => {
+  console.log(props.chosenToFavoritesAttractions)
+  return (
+    <div>
+      <h1>Favorites</h1>
+      <p>{props.chosenToFavoritesAttractions.map(attraction => attraction.attraction.name)}</p>
+    </div>
+  )
+}
+export default connect(mapStateToProps)(Favorites)
