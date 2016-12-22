912a02546431beae064fa5ba63d0b9a5d73c6243
ajankowska <ajankowska0404@gmail.com>
2016-12-22 16:36:03 +0100

    Added feature on place compare - highlight the lowest price

--- src/PlaceCompare/AttractionView/AttractionView.js
+++ src/PlaceCompare/AttractionView/AttractionView.js
@@ -4,7 +4,7 @@ import {Button} from 'react-bootstrap'
 import './AttractionView.css'
 
 import {attractions} from '../../Database'
-import {additionals} from '../../Database'
+
 
 import {ReservationButton} from './ReservationButton'
 
@@ -14,39 +14,46 @@ class AttractionView extends React.Component {
 
     const additional = this.props.thing.additional
     return (
-      <div className={this.props.theLowestPrice === additional.price ? 'the-lowest-price': 'other-price'}>
-        <p>{this.props.thing.attraction.name}</p>
 
+      <div className={this.props.theLowestPrice === additional.price ? 'the-lowest-price' : 'other-price'}>
+        <p>
+          {
+            this.props.thing.attraction.name
+          }
+        </p>
 
-            <p>
-              {
-                this.props.theLowestPrice === additional.price ?
-                  <strong>{additional.price}</strong> :
-                  additional.price
-              }
-            </p>
-
+        <p>
+          {
+            this.props.theLowestPrice === additional.price ?
+              <strong>{additional.price}</strong> :
+              additional.price
+          }
+        </p>
 
         <p>{this.props.thing.place.name}</p>
 
+        <div>
+          <p>{additional.availability}</p>
+          <p>{additional.children === true ? 'yes' : 'no'}</p>
+          <p>{additional.content}</p>
+          <p>{additional.ranking}</p>
+          <p>{additional.opinion}</p>
+        </div>
 
-            <div>
-              <p>{additional.availability}</p>
-              <p>{additional.children === true ? 'yes':'no'}</p>
-              <p>{additional.content}</p>
-              <p>{additional.ranking}</p>
-              <p>{additional.opinion}</p>
-            </div>
-
-
-        <p>{
-          attractions.filter(attraction => this.props.thing.place.attractions.indexOf(attraction.id) !== -1
-        ).map(attraction => <li key={attraction.id}>{attraction.name}</li>)
-        }</p>
+        <p>
+          {
+            attractions.filter(attraction =>
+              this.props.thing.place.attractions.indexOf(attraction.id) !== -1
+            ).map(attraction => <li key={attraction.id}>{attraction.name}</li>)
+          }
+        </p>
 
         <Button onClick={() =>
-          this.props.addAttractionToFavorites(this.props.thing.attraction, this.props.thing.place)}>ADD TO FAVORITES</Button>
+          this.props.addAttractionToFavorites(this.props.thing.attraction, this.props.thing.place)}>
+          ADD TO FAVORITES</Button>
+
         <Button>VIEW MORE</Button>
+
         <ReservationButton />
       </div>
     )
@@ -54,7 +61,3 @@ class AttractionView extends React.Component {
 }
 
 export default AttractionView
-//wyświetlenie innych atrakcji
-//podświetlenie najniższej ceny
-//formularz
-//widok ulubionych
