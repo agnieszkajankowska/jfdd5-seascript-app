16d6508d2780027bd743ba5fb24c4642e777e713
agnieszkajankowska <ajankowska0404@gmail.com>
2017-01-04 15:00:02 +0100

    Correcting menu, adding dashboard view

--- src/App/DashboardView/DashboardView.js
+++ src/App/DashboardView/DashboardView.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import {Thumbnail} from "react-bootstrap"
+import Logo from "../Navigation/logo.svg"
+import './DashboardView.css'
 
 export default () => (
-  <div className="lel">
-    <h1>Welcome lel</h1>
+  <div className="dashboard">
+    <img src={Logo} alt="logo" className="logo-dashboard"/>
+    <h1 className="title-dashboard">Wave</h1>
+    <h2>The world's first-class water sports & leisure app</h2>
+    <h3>Choose the water sports in the Form Tab and follow the arrows</h3>
   </div>
 )
+
