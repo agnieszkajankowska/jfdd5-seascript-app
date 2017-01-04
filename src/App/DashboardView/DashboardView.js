675e6ff7fee44b8adf75aea662462a012955f306
agnieszkajankowska <ajankowska0404@gmail.com>
2017-01-04 15:57:23 +0100

    Correcting menu, changing dashboard view

--- src/App/DashboardView/DashboardView.js
+++ src/App/DashboardView/DashboardView.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import {Thumbnail} from "react-bootstrap"
+import {Button} from "react-bootstrap"
 import Logo from "../Navigation/logo.svg"
 import './DashboardView.css'
+import { Link } from 'react-router'
 
 export default () => (
   <div className="dashboard">
-    <img src={Logo} alt="logo" className="logo-dashboard"/>
+    <div className="bla">
+      <img src={Logo} alt="logo" className="logo-dashboard"/>
+    </div>
     <h1 className="title-dashboard">Wave</h1>
     <h2>The world's first-class water sports & leisure app</h2>
-    <h3>Choose the water sports in the Form Tab and follow the arrows</h3>
+    <h3>Choose the water sports in the <Button bsStyle="primary">
+      <Link to='/form'>Form</Link>
+    </Button> and follow the arrows</h3>
   </div>
 )
 
