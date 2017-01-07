d36696c5b920925e2cbbd0da0a3f2535e8aebc4e
Tomasz Gola <gola92@gmail.com>
2017-01-07 22:56:20 +0100

    zabawa u Michała w ogródku

--- src/state/weather/actionCreators.js
+++ src/state/weather/actionCreators.js
@@ -1,7 +1,17 @@
 import {FETCH_WEATHER_API} from './actionTypes'
+import {places} from '../../Database'
 
 const api_key = 'dc2f2e72b22d9a90fd58cf8ed86be518'
-let city_name = 'Gdańsk'
+
+let city_names = (
+  places.map(
+    place =>
+    place.name
+  )
+)
+
+const city_name = city_names[3]
+
 
 export const fetchWeather = () => dispatch => {
   fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&APPID=" + api_key + "&units=metric").then(
