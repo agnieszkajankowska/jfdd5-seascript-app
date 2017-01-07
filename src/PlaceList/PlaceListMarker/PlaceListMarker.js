df70aea3283442e39571f37cf1e6c89ed6929e54
Tomasz Gola <gola92@gmail.com>
2017-01-07 13:43:07 +0100

    czerwone znaczniki na mapie

--- src/PlaceList/PlaceListMarker/PlaceListMarker.js
+++ src/PlaceList/PlaceListMarker/PlaceListMarker.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default (props) => (
-  <div style={{
-    background: '#fff',
-    width: 30,
-    height: 30
-  }}>dupa</div>
+    <img src={process.env.PUBLIC_URL + '/images/icons/iconmonstr-pin-12.svg'}
+       alt = ""
+         style={{
+           height:30,
+           position: 'absolute',
+           left: -15,
+           top: -30,
+           fill: 'pink'
+         }}
+    />
 )
